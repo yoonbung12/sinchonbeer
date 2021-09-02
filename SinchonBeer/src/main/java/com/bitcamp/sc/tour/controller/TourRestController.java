5983dd818070d43f3631d85e31a205f2b6f9ea3d
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-09-02 18:19:37 +0900

    tour 예약 날짜 인원 값 넘기기 / mysql order check 조건 추가

--- SinchonBeer/src/main/java/com/bitcamp/sc/tour/controller/TourRestController.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/tour/controller/TourRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bitcamp.sc.tour.service.TourService;
 import com.bitcamp.sc.tour.service.impl.TourServiceImpl;
@@ -19,4 +20,7 @@ public class TourRestController {
 		System.out.println(date);
 		return service.selectCount(date);
 	}
+	
+
+	
 }
