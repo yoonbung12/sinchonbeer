c1a4a91436ca18f20f7808a71d596351dcf75b46
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-09-02 20:35:03 +0900

    Create OrderInfo.java

---
+++ SinchonBeer/src/main/java/com/bitcamp/sc/order/domain/OrderInfo.java
@@ -0,0 +1,34 @@
+package com.bitcamp.sc.order.domain;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderInfo {
+	private int idx;
+	private Timestamp date;
+	private String category;
+	private int price;
+	private int tourIdx;
+	private int tourPeople;
+	private int memberIdx;
+	private int addressIdx;
+	
+	public OrderInfo(String category, int price, int tourIdx, int tourPeople, int memberIdx, int addressIdx) {
+		this.category = category;
+		this.price = price;
+		this.tourIdx = tourIdx;
+		this.tourPeople = tourPeople;
+		this.memberIdx = memberIdx;
+		this.addressIdx = addressIdx;
+	}
+}
