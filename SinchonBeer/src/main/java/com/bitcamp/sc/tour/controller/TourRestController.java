82f871b1ec574b2ad2c442e19299a2545d4a4317
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-09-02 15:28:05 +0900

    tour예약 가능 인원 추가

--- SinchonBeer/src/main/java/com/bitcamp/sc/tour/controller/TourRestController.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/tour/controller/TourRestController.java
@@ -1,8 +1,22 @@
 package com.bitcamp.sc.tour.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bitcamp.sc.tour.service.TourService;
+import com.bitcamp.sc.tour.service.impl.TourServiceImpl;
 
 @RestController
 public class TourRestController {
-
+	
+	@Autowired
+	TourService service;
+	
+	@GetMapping("/tour/count")
+	public int getCount(@RequestParam("mid") String date) {
+		System.out.println(date);
+		return service.selectCount(date);
+	}
 }
