9b8aba499e4625edda79e9119c2b474cacd229b6
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-09-03 18:28:32 +0900

    test: MemoryOrderDao 테스트 생성

--- SinchonBeer/src/test/java/com/bitcamp/sc/order/repository/impl/MemoryOrderDaoTest.java
+++ SinchonBeer/src/test/java/com/bitcamp/sc/order/repository/impl/MemoryOrderDaoTest.java
@@ -12,14 +12,14 @@ import org.junit.jupiter.api.Test;
 import com.bitcamp.sc.order.domain.OrderInfo;
 
 class MemoryOrderDaoTest {
-	
+
 	MemoryOrderDao dao;
-	
+
 	@BeforeEach
 	void beforeEach() {
 		dao = new MemoryOrderDao();
 	}
-	
+
 	@AfterEach
 	void afterEach() {
 		dao.clearStore();
@@ -28,25 +28,27 @@ class MemoryOrderDaoTest {
 	@Test
 	void 주문등록() {
 		// given
-		OrderInfo order = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx, addressIdx 
-		
+		OrderInfo order = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx,
+																	// addressIdx
+
 		// when
 		dao.save(order);
-		
+
 		// then
 		assertThat(order).isEqualTo(dao.findByIdx(order.getIdx()));
 	}
-	
+
 	@Test
 	void 주문조회() {
 		// given
-		OrderInfo order = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx, addressIdx 
+		OrderInfo order = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx,
+																	// addressIdx
 		dao.save(order);
-		
+
 		// when
 		OrderInfo findOrder = new OrderInfo();
 		findOrder = dao.findByIdx(order.getIdx());
-		
+
 		// then
 		assertThat(findOrder).isEqualTo(order);
 	}
@@ -54,13 +56,14 @@ class MemoryOrderDaoTest {
 	@Test
 	void 주문조회_실패() {
 		// given
-		OrderInfo order = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx, addressIdx 
+		OrderInfo order = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx,
+																	// addressIdx
 		dao.save(order);
-		
+
 		// when
 		OrderInfo findOrder = new OrderInfo();
 		findOrder = dao.findByIdx(-1);
-		
+
 		// then
 		assertThat(findOrder).isNotEqualTo(order);
 	}
@@ -68,15 +71,17 @@ class MemoryOrderDaoTest {
 	@Test
 	void 멤버인덱스로_주문조회() {
 		// given
-		OrderInfo order1 = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx, addressIdx 
-		OrderInfo order2 = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx, addressIdx 
+		OrderInfo order1 = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx,
+																		// addressIdx
+		OrderInfo order2 = new OrderInfo("shop", 22000, 1, 3, 2, 1); // category, price, tourIdx, tourPeople, memberIdx,
+																		// addressIdx
 		dao.save(order1);
 		dao.save(order2);
-		
+
 		// when
 		List<OrderInfo> findOrders = new ArrayList<>();
 		findOrders = dao.findByMemberIdx(2);
-		
+
 		// then
 		assertThat(findOrders.get(0)).isEqualTo(order1);
 		assertThat(findOrders.get(1)).isEqualTo(order2);
