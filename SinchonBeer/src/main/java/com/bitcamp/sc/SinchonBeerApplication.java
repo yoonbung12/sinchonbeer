fb1071de881cc3aae1eda3e148dd8abedfa4e55e
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-09-04 22:16:03 +0900

    exMember test 삭제

--- SinchonBeer/src/main/java/com/bitcamp/sc/SinchonBeerApplication.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/SinchonBeerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.bitcamp.sc.tour.dao")
 public class SinchonBeerApplication {
 
 	public static void main(String[] args) {
