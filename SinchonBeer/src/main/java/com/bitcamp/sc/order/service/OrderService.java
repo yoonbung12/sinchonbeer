a454df6fb8ac2a1eaec65f66261ab2fd78504c04
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-09-06 18:24:08 +0900

    refactor: OrderService 인터페이스 변경

--- SinchonBeer/src/main/java/com/bitcamp/sc/order/service/OrderService.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/order/service/OrderService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.bitcamp.sc.order.domain.OrderInfo;
 
 public interface OrderService {
-	int createOrder(String type, OrderInfo orderInfo); // 새로운 주문을 생성
+	int createOrder(String type, OrderInfo orderInfo); // 새로운 주문을 생성 
 	OrderInfo getOrderInfo(int orderIdx); // 주문 번호로 주문정보를 받아옴
 	List<OrderInfo> getOrdersInfos(String type, String memberId); // 유저 아이디로 주문정보 리스트를 받아옴
 }
