d3ad556beac6bceb877dd979a61f11c36489f480
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-09-08 19:56:41 +0900

    faet: kakaoPay 기능 구현

--- SinchonBeer/src/main/java/com/bitcamp/sc/pay/service/impl/type/KakaoPay.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/pay/service/impl/type/KakaoPay.java
@@ -19,7 +19,7 @@ import com.bitcamp.sc.pay.domain.KakaoPayReady;
 @Service
 public class KakaoPay {
 
-	private static final String HOST = "https://kapi.kakao.com";
+	private static final String HOST = "http://localhost:8080/";
 	
 	private KakaoPayReady kakaoPayReady;
 	private KakaoPayApproval kakaoPayApproval;
@@ -35,20 +35,20 @@ public class KakaoPay {
 		
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
 		params.add("cid", "TC0ONETIME");
-        params.add("partner_order_id", "1001");
+        params.add("partner_order_id", "9");
         params.add("partner_user_id", "gorany");
         params.add("item_name", "갤럭시S9");
         params.add("quantity", "1");
         params.add("total_amount", "2100");
         params.add("tax_free_amount", "100");
-        params.add("approval_url", "http://localhost:8080/kakaoPaySuccess");
-        params.add("cancel_url", "http://localhost:8080/kakaoPayCancel");
-        params.add("fail_url", "http://localhost:8080/kakaoPaySuccessFail");
+        params.add("approval_url", HOST + "kakaoPaySuccess");
+        params.add("cancel_url", HOST + "kakaoPayCancel");
+        params.add("fail_url", HOST + "kakaoPaySuccessFail");
 		
         HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<MultiValueMap<String, String>>(params, headers);
         
         try {
-			kakaoPayReady = restTemplate.postForObject(new URI(HOST + "/v1/payment/ready"), body, KakaoPayReady.class);
+			kakaoPayReady = restTemplate.postForObject(new URI("https://kapi.kakao.com/v1/payment/ready"), body, KakaoPayReady.class);
 			
 			return kakaoPayReady.getNext_redirect_pc_url();
 		} catch (RestClientException e) {
@@ -72,7 +72,7 @@ public class KakaoPay {
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
 		params.add("cid", "TC0ONETIME");
 		params.add("tid", kakaoPayReady.getTid());
-        params.add("partner_order_id", "1001");
+        params.add("partner_order_id", "9");
         params.add("partner_user_id", "gorany");
         params.add("pg_token", pg_token);
         params.add("total_amount", "2100");
@@ -80,7 +80,7 @@ public class KakaoPay {
         HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<MultiValueMap<String, String>>(params, headers);
         
         try {
-			kakaoPayApproval = restTemplate.postForObject(new URI(HOST + "/v1/payment/approve"), body, KakaoPayApproval.class);
+			kakaoPayApproval = restTemplate.postForObject(new URI("https://kapi.kakao.com/v1/payment/approve"), body, KakaoPayApproval.class);
 			
 			return kakaoPayApproval;			
 		} catch (RestClientException e) {
