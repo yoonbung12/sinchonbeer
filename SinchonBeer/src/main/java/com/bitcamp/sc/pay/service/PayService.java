110d927bff7af0bfa2e2f903fb23592a424d0196
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-09-08 19:57:04 +0900

    feat: 결제 기능 구현

--- SinchonBeer/src/main/java/com/bitcamp/sc/pay/service/PayService.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/pay/service/PayService.java
@@ -1,5 +1,14 @@
 package com.bitcamp.sc.pay.service;
 
+import java.util.List;
+
+import com.bitcamp.sc.pay.domain.PayInfo;
+
 public interface PayService {
+	
+	public int savePayment(PayInfo payInfo);
+	public PayInfo getPayInfo(int payIdx);
+	public List<PayInfo> getPayInfos(int memberIdx);
+	public List<PayInfo> getPayInfosByType(String type, int memberIdx);
 
 }
