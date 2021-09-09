2183c9e66a2f02f758d0f9ced98db4e73970ec82
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-09-09 20:42:19 +0900

    login ajax + referer 기능 구현

--- SinchonBeer/src/main/java/com/bitcamp/sc/member/controller/LoginController.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/member/controller/LoginController.java
@@ -1,11 +1,15 @@
 package com.bitcamp.sc.member.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,22 +37,25 @@ public class LoginController {
 	// 원래
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseBody
-	public Object login(
-			@RequestParam("memail") String email, 
-			@RequestParam("mpw") String pw,
-			@RequestParam(value = "redirectUri", required = false) String redirectUri, 
+	public Map<String,Object> login(
+			@RequestBody Map<String,Object> params, 
 			@RequestParam(value = "reEmail", required = false) String reEmail, 
 			HttpSession session, //로그인 정보에 대한 session
 			HttpServletResponse response, //쿠키를 받기위한 response
-			Model model
+			Model model 
 			) {
-		System.out.println(email+": "+pw);
+		System.out.println(params);
+		System.out.println(params.get("memail")+": "+params.get("mpw"));
+		System.out.println(params.get("redirectUri"));
+		Map<String,Object> map = new HashMap<>();
 		//model.addAttribute();
+		map.put("refererUri", (String)params.get("redirectUri"));
 		
 		//사용자가 입력한 정보를 서비스에서 처리하고 결과 받아오기
-		Boolean loginChk = loginService.login(email, pw, reEmail, session, response);
+		Boolean loginChk = loginService.login((String)params.get("memail"), (String)params.get("mpw"), reEmail, session, response);
+		map.put("result", loginChk);
 		
-		return model;
+		return map;
 	}
 	
 	
