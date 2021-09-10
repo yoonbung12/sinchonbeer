22ab7dec232e2fb4f25b81fc1f6e09e3630c479d
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-09-10 23:00:37 +0900

    GoodVO
    GoodsVO

---
+++ SinchonBeer/src/main/java/com/bitcamp/sc/shop/domain/GoodsVO.java
@@ -0,0 +1,24 @@
+package com.bitcamp.sc.shop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter 
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GoodsVO {
+
+	
+	private int idx; // 상품번호
+	private String gname; // 상품이름
+	private int price; //상품가격
+	private String photo; //상품이미지
+	private String text; // 상품 상세 설명
+	
+	
+}
