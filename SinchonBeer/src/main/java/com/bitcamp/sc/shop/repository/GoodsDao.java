c107aa99caf22e1be873125007910bf63bc0ebc9
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-09-11 20:37:06 +0900

    GoodsDao
    GoodsDao

---
+++ SinchonBeer/src/main/java/com/bitcamp/sc/shop/repository/GoodsDao.java
@@ -0,0 +1,16 @@
+package com.bitcamp.sc.shop.repository;
+
+import java.util.List;
+
+import com.bitcamp.sc.shop.domain.GoodsVO;
+
+public interface GoodsDao {
+	
+	
+	// 01.상품추가
+	public GoodsVO insertGoods(GoodsVO vo);
+	
+	// 02.상품삭제
+	public void deleteGoods(int idx);
+	
+}
