d1b4bfd267cd30cec527f24578a578bc79aed58d
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-09-12 16:06:18 +0900

    MyatisGoodsDao
    MybaatisGoodsDao

--- SinchonBeer/src/main/java/com/bitcamp/sc/shop/repository/impl/MybatisGoodsDao.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/shop/repository/impl/MybatisGoodsDao.java
@@ -19,18 +19,25 @@ public class MybatisGoodsDao implements GoodsDao {
 		this.template = template;
 	}
 	
-	
+	// 1.상품추가
 	@Override
 	public GoodsVO insertGoods(GoodsVO vo) {
 		template.insert(NAME_SPACE + ".insertGoods", vo);
 		return vo;
 	}
 
-
+	// 2.상품 삭제
 	@Override
 	public void deleteGoods(int idx) {
 		template.delete(NAME_SPACE + ".deleteGoods", idx);
 		
+	}
+
+
+	// 3.샹폼 챶기
+	@Override
+	public GoodsVO findIdx(int idx) {
+		return template.selectOne(NAME_SPACE + ".findIdx", idx);
 	}
 
 
