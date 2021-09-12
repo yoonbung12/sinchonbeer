675b6871b41599ad201732808f39073e0fc55540
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-09-12 16:05:59 +0900

    CartVO
    CartVo

---
+++ SinchonBeer/src/main/java/com/bitcamp/sc/cart/domain/CartVO.java
@@ -0,0 +1,21 @@
+package com.bitcamp.sc.cart.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CartVO {
+
+	private int idx; // 장바구니 번호
+	private int count; // 수량
+	private int memberIdx; // 멤버 번호
+	private int goodsIdx; // 상품 번호
+	
+}
