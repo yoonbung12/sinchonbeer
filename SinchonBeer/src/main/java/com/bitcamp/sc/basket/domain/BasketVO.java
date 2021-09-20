f3e849bf859baf7bbb22ef00a2a695f64b4bd1be
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-09-20 12:38:42 +0900

    BasketVo,Dto
    BasketVo,Dto

--- SinchonBeer/src/main/java/com/bitcamp/sc/basket/domain/BasketVO.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/basket/domain/BasketVO.java
@@ -1,28 +1,17 @@
 package com.bitcamp.sc.basket.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @ToString
-public class BasketVO {
+public class BasketVo {
 
-	private int idx; // 장바구니 번호
-	private int count; // 수량
-	private int memberIdx; // 멤버 번호
-	private int goodsIdx; // 상품 번호
-	
-	
-	public BasketVO(int count, int memberIdx, int goodsIdx) {
-		this.count = count;
-		this.memberIdx = memberIdx;
-		this.goodsIdx = goodsIdx;
-	}
-	
+	private int bidx;
+	private int midx;
+	private int gidx;
+	private String gname;
+	private String gphoto;
+	private int bcount;
+	private int gprice;
 }
