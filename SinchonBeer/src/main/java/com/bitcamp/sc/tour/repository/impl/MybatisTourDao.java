571e8620d5484da250def5e58a9946f743425bdf
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-09-21 20:20:48 +0900

    투어 예약 인원 변경 부분 수정

--- SinchonBeer/src/main/java/com/bitcamp/sc/tour/repository/impl/MybatisTourDao.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/tour/repository/impl/MybatisTourDao.java
@@ -1,6 +1,5 @@
 package com.bitcamp.sc.tour.repository.impl;
 
-import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,19 +23,19 @@ public class MybatisTourDao implements TourDao {
 
 	// 선택한 날짜의 예약 가능한 인원을 가져오는 메서드
 	@Override
-	public int selectCount(Date date) {
+	public int selectCount(String date) {
 		return template.selectOne(NAME_SPACE + ".selectCount", date);
 	}
 	
 	// 투어 예약 날짜로 투어 번호 가져오기
 	@Override
-	public int getTidxbyTdate(Date date) {
+	public int getTidxbyTdate(String date) {
 		return template.selectOne(NAME_SPACE+".getTidxByTdate",date);
 	}
 
 	// 새로운 예약, 예약 변경시 바뀌는 날짜
 	@Override
-	public int addTourPeople(int tourPeople, Date newDate) {
+	public int addTourPeople(int tourPeople, String newDate) {
 		System.out.println("인원 더하기");
 		Map<String, Object> params = new HashMap<>();
 		params.put("tourPeople", tourPeople);
@@ -46,7 +45,7 @@ public class MybatisTourDao implements TourDao {
 
 	// 예약 취소 , 예약 변경시 기존 날짜
 	@Override
-	public int subTourPeople(int tourPeople, Date currnetDate) {
+	public int subTourPeople(int tourPeople, String currnetDate) {
 		System.out.println("인원 감소");
 		Map<String, Object> params = new HashMap<>();
 		System.out.println(tourPeople+" : "+currnetDate);
@@ -67,7 +66,7 @@ public class MybatisTourDao implements TourDao {
 
 	// 주문 테이블의 예약 날짜를 변경 --> 현재는 test code
 	@Override
-	public int changeDateByMidx(int midx, Date newDate) {
+	public int changeDateByMidx(int midx, String newDate) {
 		Map<String, Object> params = new HashMap<>();
 		params.put("midx", midx);
 		params.put("tdate", newDate);
@@ -77,6 +76,15 @@ public class MybatisTourDao implements TourDao {
 
 	public String getTourDateByTidx(int tidx) {
 		return template.selectOne(NAME_SPACE+".getTourDateByTidx",tidx);
+	}
+
+	@Override
+	public int modifyTour(int tourPeople, String newDate, String resDate) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("tourPeople", tourPeople);
+		params.put("newDate", newDate);
+		params.put("resDate",resDate);
+	return	template.update(NAME_SPACE+".changeTour", params);
 	}
 
 	
