bd0c005e22828c69bb8512e93b9e82bfa9e00c4b
daayeah <daayeah515@gmail.com>
2021-09-23 10:14:27 +0900

    mypage 수정

--- SinchonBeer/src/main/java/com/bitcamp/sc/mypage/repository/MemberManagerDao.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/mypage/repository/MemberManagerDao.java
@@ -1,17 +1,26 @@
 package com.bitcamp.sc.mypage.repository;
 
-import java.util.List;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
-import com.bitcamp.sc.member.domain.LoginInfo;
 import com.bitcamp.sc.member.domain.Member;
 
-public interface MemberManagerDao {
-	// 회원 정보 조회
-	List<Member> readMember(int idx);
+@Repository
+public class MemberManagerDao {
+	private static final String namespace = "MypageMapper";
+	private final SqlSessionTemplate template;
+
+	public MemberManagerDao(SqlSessionTemplate template) {
+		this.template = template;
+	}
 
 	// 회원 정보 수정
-	public void updateMember(Member member);
+	public void updateMember(Member member) {
+		template.update(namespace + ".updateMember", member);
+	}
 
 	// 회원 탈퇴
-	public void deleteMember(Member member);
+	public void deleteMember(Member member) {
+		template.delete(namespace + ".deleteMember", member);
+	}
 }
