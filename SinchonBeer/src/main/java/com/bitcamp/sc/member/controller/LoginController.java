eac2842d7304350e03b641fbd5461976a5c63cfd
silversoren <ehsoren9@gmail.com>
2021-09-23 20:04:11 +0900

    로그인 쿠키 설정 수정

--- SinchonBeer/src/main/java/com/bitcamp/sc/member/controller/LoginController.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/member/controller/LoginController.java
@@ -9,14 +9,15 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.bitcamp.sc.member.memberService.LoginService;
+import com.bitcamp.sc.member.domain.LoginInfo;
+import com.bitcamp.sc.member.service.LoginService;
 
 
 @Controller
@@ -28,21 +29,25 @@ public class LoginController {
 	@RequestMapping( method = RequestMethod.GET)
 	public String loginForm(
 			//로그인 성공시 되돌아 갈 이전 페이지
-			@RequestHeader(value = "referer", required = false) String redirectUri, 
+			@RequestHeader(value = "referer", required = false) String redirectUri,
+			@CookieValue(value="reEmail", required = false) String reEmail,
+			HttpSession session,
 			Model model
 			) {
+		//LoginInfo loginInfo = (LoginInfo)getSession();
 		model.addAttribute("redirectUri", redirectUri);
+		model.addAttribute("reEmail", reEmail);
 		return "/member/loginForm";
 	}
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String,Object> login(
-			@RequestBody Map<String,Object> params, 
-			@RequestParam(value = "reEmail", required = false) String reEmail, 
+			@RequestBody Map<String,Object> params,
 			HttpSession session, //로그인 정보에 대한 session
 			HttpServletResponse response //쿠키를 받기위한 response
 			//Model model 
 			) {
+		System.out.println("reEmail : " + params.get("reEmail"));
 		System.out.println(params);
 		System.out.println(params.get("memail")+": "+params.get("mpw"));
 		System.out.println(params.get("redirectUri"));
@@ -50,7 +55,7 @@ public class LoginController {
 		map.put("refererUri", (String)params.get("redirectUri"));
 		
 		//사용자가 입력한 정보를 서비스에서 처리하고 결과 받아오기
-		Boolean loginChk = loginService.login((String)params.get("memail"), (String)params.get("mpw"), reEmail, session, response);
+		Boolean loginChk = loginService.login((String)params.get("memail"), (String)params.get("mpw"), (String)params.get("reEmail"), session, response);
 		map.put("result", loginChk);
 		
 		return map;
