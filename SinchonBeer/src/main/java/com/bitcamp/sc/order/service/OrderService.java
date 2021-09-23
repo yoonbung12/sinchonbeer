5e7a0cc0dc1b59963a810f07f958c33de653a373
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-09-23 10:56:04 +0900

    feat: #62 주문 인터페이스 기능 추가

--- SinchonBeer/src/main/java/com/bitcamp/sc/order/service/OrderService.java
+++ SinchonBeer/src/main/java/com/bitcamp/sc/order/service/OrderService.java
@@ -8,7 +8,8 @@ public interface OrderService {
 	int createOrder(String type, OrderInfo orderInfo); // 새로운 주문을 생성
 	OrderInfo getOrderInfo(int orderIdx); // 주문 번호로 주문정보를 받아옴
 	List<OrderInfo> getOrderInfos(int memberIdx); // 유저 아이디로 주문정보 리스트를 받아옴
-	List<OrderInfo> getOrderInfosByType(String type, int memberId); // 주문 카테고리와 유저 아이디로 주문정보 리스트를 받아옴
+	List<OrderInfo> getOrderInfosByType(String type, int memberIdx); // 주문 카테고리와 유저 아이디로 주문정보 리스트를 받아옴
 	int deleteOrder(int idx);
 	int confirmOrder(int idx);
+	int changeOrderStatus(int idx, String status);
 }
